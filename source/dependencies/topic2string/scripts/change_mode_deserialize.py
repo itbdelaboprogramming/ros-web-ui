570a2bc368b060db28b682efb055eaff12bbdec8
farhan-sw <farhansw.off@gmail.com>
2025-03-16 17:49:33 +0700

    feat: add change mode support

---
+++ source/dependencies/topic2string/scripts/change_mode_deserialize.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import rospy
+import json
+from std_msgs.msg import String
+from msd700_msg.msg import SwitchModeMsg
+from msd700_msg.srv import SwitchMode
+
+class ChangeModeServiceNode:
+    def __init__(self):
+        rospy.init_node('change_mode_service_node', anonymous=False)
+        
+        self.input_topic = rospy.get_param('~input_topic', '/string/change_mode')
+        self.service_name = rospy.get_param('~service_name', '/switch_mode')
+        
+        self.sub = rospy.Subscriber(self.input_topic, String, self.callback)
+        
+        rospy.loginfo("ChangeModeServiceNode aktif. Subscribe: %s, Service: %s", 
+                      self.input_topic, self.service_name)
+        
+        # tmeout service
+        rospy.wait_for_service(self.service_name, timeout=5.0)
+        self.switch_mode_srv = rospy.ServiceProxy(self.service_name, SwitchMode)
+    
+    def callback(self, msg):
+        try:
+            # Deserialisasi pesan JSON menjadi dictionary
+            data = json.loads(msg.data)
+            rospy.loginfo("Data JSON diterima: %s", data)
+        except json.JSONDecodeError as e:
+            rospy.logerr("Gagal mendeserialisasi JSON: %s", e)
+            return
+        
+        # Konversi dictionary menjadi objek SwitchModeMsg
+        switch_mode_msg = SwitchModeMsg()
+        switch_mode_msg.mode = data.get("mode", "")
+        switch_mode_msg.open_rviz = data.get("open_rviz", False)
+        switch_mode_msg.use_simulator = data.get("use_simulator", False)
+        switch_mode_msg.map_file = data.get("map_file", "")
+        switch_mode_msg.point_mode = data.get("point_mode", "")
+        
+        # Panggil service
+        try:
+            response = self.switch_mode_srv(switch_mode_msg)
+            rospy.loginfo("Respon service: success=%s, message=%s", 
+                          response.success, response.message)
+        except rospy.ServiceException as e:
+            rospy.logerr("Pemanggilan service gagal: %s", e)
+
+if __name__ == '__main__':
+    try:
+        node = ChangeModeServiceNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
---
+++ source/dependencies/topic2string/scripts/change_mode_serialize.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import rospy
+import json
+from msd700_msg.msg import SwitchModeMsg
+from std_msgs.msg import String
+
+class ChangeModeSerializeNode:
+    def __init__(self):
+        rospy.init_node('change_mode_serialize_node', anonymous=False)
+        
+        self.input_topic = rospy.get_param('~input_topic', '/server/change_mode')
+        self.output_topic = rospy.get_param('~output_topic', '/string/change_mode')
+        
+        # Buat publisher dan subscriber
+        self.pub = rospy.Publisher(self.output_topic, String, queue_size=10)
+        self.sub = rospy.Subscriber(self.input_topic, SwitchModeMsg, self.callback)
+        
+        rospy.loginfo("ChangeModeSerializeNode aktif. Subscribe: %s, Publish: %s", 
+                      self.input_topic, self.output_topic)
+    
+    def callback(self, msg):
+        # Konversi pesan SwitchModeMsg ke dictionary
+        data = {
+            "mode": msg.mode,
+            "open_rviz": msg.open_rviz,
+            "use_simulator": msg.use_simulator,
+            "map_file": msg.map_file,
+            "point_mode": msg.point_mode
+        }
+        # Serialisasi ke string JSON
+        json_str = json.dumps(data)
+
+        self.pub.publish(json_str)
+        rospy.loginfo("JSON dikirim: %s", json_str)
+
+if __name__ == '__main__':
+    try:
+        node = ChangeModeSerializeNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
