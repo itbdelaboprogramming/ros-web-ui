34417a15295e09a473360aefc5825a9b915c5064
farhan-sw <farhansw.off@gmail.com>
2025-03-17 15:46:19 +0700

    feat: update docker file mount volume

--- docker.sh
+++ docker.sh
@@ -5,6 +5,7 @@ USERNAME="itbdelabo"
 docker run --rm -it \
   --network host \
   -v $PWD/Certificates:/home/$USERNAME/ros-web-ui-ws/src/ros-web-ui/source/dependencies/aws_mqtt/certs/ \
+  -v $PWD/source:/home/$USERNAME/ros-web-ui-ws/src/ \
   -v /home/ubuntu/ros_maps:/ros_maps \
   -e MAPS_FOLDER="/ros_maps" \
   ros-container
